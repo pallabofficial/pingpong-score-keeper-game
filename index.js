60cfa28f581a388627d7e84c7fb3620b2bda1c1f
pallabofficial <officialpallabmondal000@gmail.com>
2024-01-06 19:52:28 +0530

    added winning score and winner criteria:- added javascript codes to decide the winner. If a player reaches certain score, it will be declared winner and the games will get stopped

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-let player1Score =  parseInt(document.querySelector("#player-1-score").innerHTML);
-let player2Score =  parseInt(document.querySelector("#player-2-score").innerHTML);
+let player1Score = 0;
+let player2Score = 0;
 
 const player1Button = document.querySelector("#player1");
+const player2Button = document.querySelector("#player2");
+
 player1Button.addEventListener("click", function(){
     player1Score++;
-    document.querySelector("#player-1-score").innerHTML = player1Score;   
+    document.querySelector("#player-1-score").innerHTML = player1Score;
+    checkWinner();
 });
 
-const player2Button = document.querySelector("#player2");
 player2Button.addEventListener("click", function(){
     player2Score++;
-    document.querySelector("#player-2-score").innerHTML = player2Score;   
+    document.querySelector("#player-2-score").innerHTML = player2Score;
+    checkWinner();
 });
 
+function checkWinner() {
+    if (player1Score === 5) {
+        document.querySelector("#player-1-score").innerHTML = "Winner";
+        endGame();
+    } else if (player2Score === 5) {
+        document.querySelector("#player-2-score").innerHTML = "Winner";
+        endGame();
+    }
+}
+
+function endGame() {
+    // You can add any logic here to handle the end of the game
+    // For example, disable the buttons or show a reset button
+    player1Button.removeEventListener("click", checkWinner);
+    player2Button.removeEventListener("click", checkWinner);
+}
